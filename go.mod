d3957dd2aa8538ae97d412629e79b648f864bdb5
Daniel <danield@Daniels-MacBook-Pro.local>
2023-08-07 22:20:06 +0200

    chi router

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/raad-dego/chirpy
 
 go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
@@ -12,20 +14,31 @@ func main() {
 		fileserverHits: 0,
 	}
 
-	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", healthHandler)
-	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	// ServeMux
+	// mux := http.NewServeMux()
+	// mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	// mux.HandleFunc("/healthz", healthHandler)
+	// mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	// corsMux := MiddlewareCors(mux)
+	// srv := &http.Server{
+	// 	Addr:    ":" + port,
+	// 	Handler: corsMux,
+	// }
+	// Chi router
+	r := chi.NewRouter()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app/*", fsHandler)
+	r.Handle("/app", fsHandler)
+	r.Get("/healthz", healthHandler)
+	r.Get("/metrics", apiCfg.metricsHandler)
 
-	corsMux := MiddlewareCors(mux)
+	corsR := MiddlewareCors(r)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsMux,
+		Handler: corsR,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
-
-
